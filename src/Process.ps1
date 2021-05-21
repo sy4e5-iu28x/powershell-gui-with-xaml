6f9627569e080cf21c94651e54bd5788a8d44702
Daisuke.M <84036660+sy4e5-iu28x@users.noreply.github.com>
2021-05-21 20:51:59 +0900

    WindowTabの差し込み、ダイアログの差し込み実装

--- src/Process.ps1
+++ src/Process.ps1
@@ -4,19 +4,34 @@ using module ".\XamlHelper.psm1"
 Add-Type -AssemblyName PresentationFramework, PresentationCore
 
 try{
-    [string]$appXamlFilePath = ".\xaml\App.xaml"
-    [ApplicationXamlHelper]$appXamlHelper = [ApplicationXamlHelper]::new($appXamlFilePath)
+    [string] $appXamlFilePath = ".\xaml\App.xaml"
     # ResourceDictionaryのSource絶対パス変換処理
-    [xml]$appXaml = $appXamlHelper.ApplyResourceDicSouce()
+    [ApplicationXamlHelper] $appXamlHelper = [ApplicationXamlHelper]::new($appXamlFilePath)
+    [xml] $appXaml = $appXamlHelper.ApplyResourceDicSouce()
     [Logger]::GetInstance().Debug("ResourceDictionaryの絶対パス変換が完了しました。")
     
-    [System.Xml.XmlNodeReader]$appReader = [System.Xml.XmlNodeReader]::new($appXaml)
-    [System.Windows.Application]$app = [System.Windows.Markup.XamlReader]::Load($appReader)
+    [System.Xml.XmlNodeReader] $appReader = [System.Xml.XmlNodeReader]::new($appXaml)
+    [System.Windows.Application] $app = [System.Windows.Markup.XamlReader]::Load($appReader)
     [Logger]::GetInstance().Debug("ApplicationインスタンスのLoadが完了しました。")
 
-    [xml]$windowXaml = Get-Content -Encoding UTF8 .\xaml\Window.xaml
-    [System.Xml.XmlNodeReader]$windowReader = [System.Xml.XmlNodeReader]::new($windowXaml)
-    [System.Windows.Window]$window = [System.Windows.Markup.XamlReader]::Load($windowReader)
+    [string] $windowXamlFilePath = ".\xaml\Window.xaml"
+    # WindowTab定義の動的追加処理
+    [WindowXamlHelper] $windowXamlHelper = [WindowXamlHelper]::new($windowXamlFilePath)
+    $windowTabXamlFilePaths = @(
+        ".\xaml\windowtabs\ManagementTabItem.xaml",
+        ".\xaml\windowtabs\DefinitionTabItem.xaml",
+        ".\xaml\windowtabs\ConfigurationTabItem.xaml"
+    )
+    $windowXamlHelper.ApplyWindowTabs($windowTabXamlFilePaths) 
+
+    # Dialog定義の動的追加処理
+    $dialogTabXamlFilePaths = @(
+        ".\xaml\dialogs\DefaultTabItem.xaml"
+    )
+    [xml] $windowXaml = $windowXamlHelper.ApplyDialogs($dialogTabXamlFilePaths)
+
+    [System.Xml.XmlNodeReader] $windowReader = [System.Xml.XmlNodeReader]::new($windowXaml)
+    [System.Windows.Window] $window = [System.Windows.Markup.XamlReader]::Load($windowReader)
     
     $app.run($window)
 } catch {
--- src/XamlHelper.psm1
+++ src/XamlHelper.psm1
@@ -11,7 +11,7 @@ class XamlCommonHelper {
     [System.Xml.XmlNamespaceManager]CreateXamlNsManager([string]$xamlFilePath) {
         [xml]$targetXaml = Get-Content -Encoding UTF8 -Path $xamlFilePath
         [System.Xml.XmlNamespaceManager]$nsManager = [System.Xml.XmlNamespaceManager]::new($targetXaml.NameTable)
-        $nsManager.AddNamespace("def", $targetXaml.DocumentElement.GetAttribute("xmlns"))
+        $nsManager.AddNamespace("ns", $targetXaml.DocumentElement.GetAttribute("xmlns"))
         $nsManager.AddNamespace("x", $targetXaml.DocumentElement.GetAttribute("xmlns:x"))
         return $nsManager
     }
@@ -34,7 +34,14 @@ class ApplicationXamlHelper {
     #>
     ApplicationXamlHelper([string] $xamlFilePath) {
         $this.filePath = $xamlFilePath
-        $this.appXaml = Get-Content -Encoding UTF8 -Path $this.filePath
+        try {
+            $this.appXaml = Get-Content -Encoding UTF8 -Path $this.filePath
+        } catch {
+            [Logger]::GetInstance().Debug($PSItem)
+            $message = "ApplicationXamlの読み込みに失敗しました。[${xamlFilePath}]"
+            [Logger]::GetInstance().Debug($message)
+            Write-Error -Message $message
+        }
     }
 
     <#
@@ -43,13 +50,15 @@ class ApplicationXamlHelper {
     #>
     [xml] ApplyResourceDicSouce() {
         try {
-            $xPath = "/def:Application/def:Application.Resources/def:ResourceDictionary/def:ResourceDictionary.MergedDictionaries/def:ResourceDictionary"
+            $xPath = "/ns:Application/ns:Application.Resources/ns:ResourceDictionary/ns:ResourceDictionary.MergedDictionaries/ns:ResourceDictionary"
 
             [XamlCommonHelper]$commonHelper = [XamlCommonHelper]::new()
             [System.Xml.XmlNamespaceManager]$nsManager = $commonHelper.CreateXamlNsManager($this.filePath)
 
             # 相対パスを絶対パスに変換する
-            [System.Xml.XmlNodeList]$this.appXaml.SelectNodes($xPath, $nsManager) | ForEach-Object {
+            [System.Xml.XmlNodeList] $tabItemNodes = $this.appXaml.SelectNodes($xPath, $nsManager)
+            [Logger]::GetInstance().Debug("TabItem:${tabItemNodes}")
+            $tabItemNodes | ForEach-Object {
                 $attributeSource = ([System.Xml.XmlNode]$_).Attributes.GetNamedItem("Source")
                 $relativePath = $attributeSource.Value
                 $currentPath = Get-Location
@@ -60,9 +69,104 @@ class ApplicationXamlHelper {
         } catch {
             [Logger]::GetInstance().Debug($PSItem)
             $message = "ResourceDictionaryの属性Sourceの絶対パス変換に失敗しました。"
-            Write-Error -Message $message
             [Logger]::GetInstance().Debug($message)
+            Write-Error -Message $message
             return $null
         }
+    }
+}
+
+<#
+.SYNOPSIS
+WindowXamlヘルパクラス
+#>
+class WindowXamlHelper {
+    Hidden [string] $filePath
+    Hidden [xml] $windowXaml
+
+    <#
+    .Description
+    コンストラクタ
+
+    .PARAMETER xamlFilePath
+    対象のapp.xamlファイルパス
+    #>
+    WindowXamlHelper([string] $xamlFilePath) {
+        $this.filePath = $xamlFilePath
+        try {
+            $this.windowXaml = Get-Content -Encoding UTF8 -Path $this.filePath
+        } catch {
+            [Logger]::GetInstance().Debug($PSItem)
+            $message = "WindowXamlの読み込みに失敗しました。[${xamlFilePath}]"
+            [Logger]::GetInstance().Debug($message)
+            Write-Error -Message $message
+        }
+    }
+
+    <#
+    .Description
+    指定ファイルパス集合から任意XPathで取得したノードを、任意XPathで指定したWindow.xaml内の任意ノードに追加する共通処理。
+
+    .PARAMETER srcFilePaths
+    対象のxamlファイルパス集合
+
+    .PARAMETER srcXPath
+    追加元のXPath
+
+    .PARAMETER destWindowXpath
+    追加先のWindow.xaml内のXPath
+    #>
+    Hidden [xml] ApplyFromTo([string[]] $srcFilePaths, [string] $srcXPath, [string] $destWindowXpath) {
+        try {
+            [XamlCommonHelper]$commonHelper = [XamlCommonHelper]::new()
+            [System.Xml.XmlNamespaceManager]$nsManager = $commonHelper.CreateXamlNsManager($this.filePath)
+            
+            $srcFilePaths | ForEach-Object {
+
+                [xml] $srcXaml = Get-Content -Encoding UTF8 -Path $_
+                [System.Xml.XmlNode] $targetNode = $srcXaml.SelectSingleNode($srcXPath, $nsManager)
+
+                if($null -eq $targetNode) {
+                    [Logger]::GetInstance().Debug("${_}は${srcXPath}の定義が見つかりませんでした。")
+                    continue
+                }
+                $tabControlXPath = $destWindowXpath
+                $dest = $this.windowXaml.SelectSingleNode($tabControlXPath, $nsManager)
+                $dest.AppendChild($this.windowXaml.ImportNode($targetNode, $true))
+            }
+            return $this.windowXaml
+        } catch {
+            [Logger]::GetInstance().Debug($PSItem)
+            $message = "${destWindowXpath}への${srcXPath}追加処理に失敗しました。"
+            [Logger]::GetInstance().Debug($message)
+            Write-Error -Message $message
+            return $null
+        }
+    }
+
+    <#
+    .SYNOPSIS
+    WindowTabControlへTabItemを追加する。
+
+    .PARAMETER xamlFilePathCollection
+    対象のxamlファイルパスのstring配列
+    #>
+    [xml] ApplyWindowTabs([string[]] $xamlFilePathCollection) {
+        $srcXPath = "/ns:TabItem"
+        $destWindowXPath = "//*[@x:Name='WindowTabControl']"
+        return $this.ApplyFromTo($xamlFilePathCollection, $srcXPath, $destWindowXPath)
+    }
+
+    <#
+    .SYNOPSIS
+    DialogTabControlへTabItemを追加する。
+
+    .PARAMETER xamlFilePathCollection
+    対象のxamlファイルパスのstring配列
+    #>
+    [xml] ApplyDialogs([string[]] $xamlFilePathCollection) {
+        $srcXPath = "/ns:TabItem"
+        $destWindowXPath = "//*[@x:Name='DialogTabControl']"
+        return $this.ApplyFromTo($xamlFilePathCollection, $srcXPath, $destWindowXPath)
     }
 }
